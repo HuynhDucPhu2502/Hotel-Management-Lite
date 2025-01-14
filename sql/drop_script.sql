5edf1b2950ec1f7c3c2abc9a565c3da410d07d3e
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-01-14 18:45:29 +0700

    drop table sql script

---
+++ sql/drop_script.sql
@@ -0,0 +1,11 @@
+USE NewHotelDatabase
+
+DROP TABLE accounts;
+DROP TABLE customers;
+DROP TABLE employees;
+DROP TABLE persons;
+DROP TABLE rooms;
+DROP TABLE room_categories;
+DROP TABLE hotel_services;
+DROP TABLE service_categories;
+DROP TABLE reservation_forms
