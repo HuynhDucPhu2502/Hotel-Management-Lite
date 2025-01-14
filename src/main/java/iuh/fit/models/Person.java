abc584fbf98ddccb9323230720ae76b33abe44ce
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-01-14 11:04:13 +0700

    create new model class

---
+++ src/main/java/iuh/fit/models/Person.java
@@ -0,0 +1,48 @@
+package iuh.fit.models;
+
+import iuh.fit.models.enums.Gender;
+import iuh.fit.models.enums.ObjectStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "persons")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Person {
+
+    @Id
+    @Column(name = "person_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long personID;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "address")
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(name = "id_card_number", nullable = false)
+    private String idCardNumber;
+
+    @Column(nullable = false)
+    private LocalDate dob;
+
+    @Column(name = "is_activate", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ObjectStatus isActivate;
+}
