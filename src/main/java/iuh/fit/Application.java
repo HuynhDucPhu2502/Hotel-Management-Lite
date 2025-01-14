3cc8853401d62ffcbf1bf535ffcd5d1c096a78a9
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-01-14 18:45:08 +0700

    block code for later use

--- src/main/java/iuh/fit/Application.java
+++ src/main/java/iuh/fit/Application.java
@@ -9,11 +9,11 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/iuh/fit/view/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/iuh/fit/view/hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
     }
 
     public static void main(String[] args) {
