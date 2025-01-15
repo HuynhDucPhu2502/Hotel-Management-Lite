585f7bd2b1b0ed100b4399dcb9828fcb340545b0
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-01-15 18:14:49 +0700

    create Invoice class

---
+++ src/main/java/iuh/fit/models/ReservationRoomDetail.java
@@ -0,0 +1,37 @@
+package iuh.fit.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Admin 1/15/2025
+ **/
+@Entity
+@Table(name = "reservation_room_details")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ReservationRoomDetail {
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "reservation_room_detail_id")
+    private String reservationRoomDetailID;
+
+    @Column(name = "date_changed", nullable = false)
+    private LocalDateTime dateChanged;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @ManyToOne
+    @JoinColumn(name = "reservation_id", nullable = false)
+    private ReservationForm reservationForm;
+}
