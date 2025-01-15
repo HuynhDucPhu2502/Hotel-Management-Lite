912b497c8aec178442b5b747bdd508df7981ab7c
phatdang <phatdang19052004@gmail.com>
2025-01-15 12:18:20 +0700

    added select query statements

--- sql/drop_script.sql
+++ sql/drop_script.sql
@@ -9,3 +9,13 @@ DROP TABLE rooms;
 DROP TABLE room_categories;
 DROP TABLE hotel_services;
 DROP TABLE service_categories;
+
+SELECT * FROM reservation_forms;
+SELECT * FROM accounts;
+SELECT * FROM hotel_services;
+SELECT * FROM employees;
+SELECT * FROM customers;
+SELECT * FROM rooms;
+SELECT * FROM room_usage_services;
+SELECT * FROM history_checkin;
+SELECT * FROM history_checkout;
