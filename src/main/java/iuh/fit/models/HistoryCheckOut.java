4496c7d776ca7892477f2231e5dc5f31f9ee9455
phatdang <phatdang19052004@gmail.com>
2025-01-15 12:22:29 +0700

    created HistoryCheckout class

---
+++ src/main/java/iuh/fit/models/HistoryCheckOut.java
@@ -0,0 +1,30 @@
+package iuh.fit.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "history_checkout")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HistoryCheckOut {
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "room_history_checkout_id")
+    private String roomHistoryCheckOutID;
+
+    @Column(name = "date_checking_out", nullable = false)
+    private LocalDateTime dateOfCheckingOut;
+
+    @OneToOne
+    @JoinColumn(name = "reservation_id")
+    private ReservationForm reservationForm;
+}
