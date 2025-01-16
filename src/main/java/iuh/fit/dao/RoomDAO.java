5d4b8a7ba405e088fc5fb9a3cc2e7894d1adf40d
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-01-16 20:57:09 +0700

    add findById method

--- src/main/java/iuh/fit/dao/RoomDAO.java
+++ src/main/java/iuh/fit/dao/RoomDAO.java
@@ -1,13 +1,28 @@
 package iuh.fit.dao;
 
-import iuh.fit.models.Customer;
 import iuh.fit.models.Room;
+import iuh.fit.utils.EntityManagerUtil;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 
 import java.util.List;
 
 public class RoomDAO {
+
+    public static Room findById(String id) {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+
+            return em.find(Room.class, id);
+
+        }  catch (Exception resourceException) {
+
+            resourceException.printStackTrace();
+            return null;
+
+        }
+    }
+
+
     public static List<Room> getData(EntityManager em){
         try{
             Query query = em.createQuery(
