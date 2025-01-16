21c97d9a8162cc063610e36ecf37f0fc405ddc5f
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-01-16 13:11:20 +0700

    new password hashing class for security

---
+++ src/main/java/iuh/fit/security/PasswordHashing.java
@@ -0,0 +1,32 @@
+package iuh.fit.security;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Admin 1/16/2025
+ **/
+public class PasswordHashing {
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(password.getBytes(StandardCharsets.UTF_16LE));
+            return bytesToHex(encodedHash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString().toUpperCase();
+    }
+}
