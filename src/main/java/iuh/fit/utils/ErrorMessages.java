43ddfbdbe1a6279049730fdefea4f719730219c6
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-03-28 23:12:45 +0700

    Thêm các message cho phần login

--- src/main/java/iuh/fit/utils/ErrorMessages.java
+++ src/main/java/iuh/fit/utils/ErrorMessages.java
@@ -4,4 +4,8 @@ package iuh.fit.utils;
  * Admin 1/16/2025
  **/
 public class ErrorMessages {
+    // Login
+    public static final String LOGIN_INVALID_USERNAME = "Tài khoản không được bỏ trống";
+    public static final String LOGIN_INVALID_PASSWORD = "Mật khẩu không được để trống";
+    public static final String LOGIN_INVALID_ACCOUNT = "Tài khoản hoặc mật khẩu không hợp lệ";
 }
