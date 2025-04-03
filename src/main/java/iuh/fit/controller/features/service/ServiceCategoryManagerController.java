b0e662f605b4b3d68b0538ea2270eea525211815
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-04-03 23:14:24 +0700

    thêm chức năng service category vào chương trinh

---
+++ src/main/java/iuh/fit/controller/features/service/ServiceCategoryManagerController.java
@@ -0,0 +1,397 @@
+package iuh.fit.controller.features.service;
+
+import com.dlsc.gemsfx.DialogPane;
+import iuh.fit.dao.ServiceCategoryDAO;
+import iuh.fit.models.ServiceCategory;
+import iuh.fit.models.enums.ObjectStatus;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class ServiceCategoryManagerController {
+
+    @FXML
+    private TextField serviceCategoryIDTextField, serviceCategoryNameTextField;
+    @FXML
+    private ComboBox<Image> iconSelector;
+
+    @FXML
+    private ComboBox<String> serviceCategoryIDSearchField;
+    @FXML
+    private TextField serviceCategoryNameSearchField;
+
+    @FXML
+    private TableView<ServiceCategory> serviceCategoryTableView;
+    @FXML
+    private TableColumn<ServiceCategory, String> serviceCategoryIDColumn;
+    @FXML
+    private TableColumn<ServiceCategory, String> serviceCategoryNameColumn;
+    @FXML
+    private TableColumn<ServiceCategory, Void> actionColumn;
+    @FXML
+    private TableColumn<ServiceCategory, String> iconColumn;
+
+    @FXML
+    private Button addBtn, resetBtn, updateBtn;
+
+    @FXML
+    private DialogPane dialogPane;
+
+    private ObservableList<ServiceCategory> items;
+    private final Map<Image, String> iconServiceMap = new HashMap<>();
+
+    public void initialize() {
+        dialogPane.toFront();
+        serviceCategoryTableView.setFixedCellSize(40);
+
+        loadData();
+        setupTable();
+
+        resetBtn.setOnAction(e -> handleResetAction());
+        addBtn.setOnAction(e -> handleAddAction());
+        updateBtn.setOnAction(e -> handleUpdateAction());
+        serviceCategoryIDSearchField.setOnAction(e -> handleSearchAction());
+        setupIconSelector();
+    }
+
+    private void loadData() {
+        Task<ObservableList<ServiceCategory>> loadDataTask = new Task<>() {
+            @Override
+            protected ObservableList<ServiceCategory> call() {
+                List<ServiceCategory> serviceCategories = ServiceCategoryDAO.findAll();
+                return FXCollections.observableArrayList(serviceCategories);
+            }
+        };
+
+        loadDataTask.setOnSucceeded(e -> {
+            items = loadDataTask.getValue();
+            serviceCategoryTableView.setItems(items);
+            serviceCategoryTableView.refresh();
+            iconSelector.getSelectionModel().selectFirst();
+
+            List<String> Ids = ServiceCategoryDAO.getTopThreeID();
+            serviceCategoryIDSearchField.getItems().setAll(Ids);
+            serviceCategoryIDTextField.setText(ServiceCategoryDAO.getNextServiceCategoryID());
+        });
+
+
+        Thread loadDataThread = new Thread(loadDataTask);
+        loadDataThread.setDaemon(true);
+        loadDataThread.start();
+    }
+
+    private void setupTable() {
+        serviceCategoryIDColumn.setCellValueFactory(new PropertyValueFactory<>("serviceCategoryID"));
+        serviceCategoryNameColumn.setCellValueFactory(new PropertyValueFactory<>("serviceCategoryName"));
+        setupActionColumn();
+        setupIconColumn();
+
+        serviceCategoryTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+    }
+
+    private void setupActionColumn() {
+        actionColumn.setCellFactory(param -> new TableCell<>() {
+            private final Button updateButton = new Button("Cập nhật");
+            private final Button deleteButton = new Button("Xóa");
+            private final HBox hBox = new HBox(10);
+
+            {
+                updateButton.getStyleClass().add("button-update");
+                deleteButton.getStyleClass().add("button-delete");
+                hBox.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/iuh/fit/styles/Button.css")).toExternalForm());
+                updateButton.setOnAction(event -> handleUpdateBtn(getTableView().getItems().get(getIndex())));
+                deleteButton.setOnAction(event -> handleDeleteAction(getTableView().getItems().get(getIndex())));
+                hBox.setAlignment(Pos.CENTER);
+                hBox.getChildren().addAll(updateButton, deleteButton);
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty || getTableRow() == null || getTableRow().getItem() == null) {
+                    setGraphic(null);
+                    return;
+                }
+
+                ServiceCategory serviceCategory = getTableRow().getItem();
+                if (serviceCategory == null) {
+                    setGraphic(null);
+                    return;
+                }
+
+                boolean hasRoomInUse = ServiceCategoryDAO.isServiceCategoryInUse(serviceCategory.getServiceCategoryID());
+                updateButton.setDisable(hasRoomInUse);
+                deleteButton.setDisable(hasRoomInUse);
+
+                setGraphic(hBox);
+            }
+        });
+    }
+
+    private void setupIconColumn() {
+        iconColumn.setCellValueFactory(new PropertyValueFactory<>("icon"));
+
+        iconColumn.setCellFactory(column -> new TableCell<>() {
+            @Override
+            protected void updateItem(String iconName, boolean empty) {
+                super.updateItem(iconName, empty);
+                if (empty || iconName == null) {
+                    setGraphic(null);
+                } else {
+                    String iconPath = "/iuh/fit/icons/service_icons/ic_" + iconName + ".png";
+                    ImageView imageView = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream(iconPath))));
+                    imageView.setFitWidth(24);
+                    imageView.setFitHeight(24);
+
+                    HBox hBox = new HBox(imageView);
+                    hBox.setAlignment(Pos.CENTER);
+                    setGraphic(hBox);
+                }
+            }
+        });
+    }
+
+    private void handleResetAction() {
+        serviceCategoryNameTextField.setText("");
+        serviceCategoryIDTextField.setText(ServiceCategoryDAO.getNextServiceCategoryID());
+        iconSelector.getSelectionModel().selectFirst();
+
+        addBtn.setManaged(true);
+        addBtn.setVisible(true);
+        updateBtn.setManaged(false);
+        updateBtn.setVisible(false);
+    }
+
+    private void handleAddAction() {
+        try {
+            Image selectedIcon = iconSelector.getSelectionModel().getSelectedItem();
+            String iconName = iconServiceMap.get(selectedIcon);
+
+            ServiceCategory serviceCategory = new ServiceCategory(
+                    serviceCategoryIDTextField.getText(),
+                    serviceCategoryNameTextField.getText(),
+                    ObjectStatus.ACTIVE,
+                    iconName
+            );
+
+            Task<Void> addTask = new Task<>() {
+                @Override
+                protected Void call() {
+                    ServiceCategoryDAO.createData(serviceCategory);
+                    return null;
+                }
+            };
+
+            addTask.setOnRunning(e -> {
+                addBtn.setDisable(true);
+                updateBtn.setDisable(true);
+            });
+
+            addTask.setOnSucceeded(e -> {
+                addBtn.setDisable(false);
+                updateBtn.setDisable(false);
+                handleResetAction();
+                loadData();
+                dialogPane.showInformation("Thêm thành công", "Loại dịch vụ đã được thêm");
+            });
+
+
+            Thread addThread = new Thread(addTask);
+            addThread.setDaemon(true);
+            addThread.start();
+
+        } catch (IllegalArgumentException e) {
+            dialogPane.showWarning("LỖI", e.getMessage());
+        }
+    }
+
+    private void handleDeleteAction(ServiceCategory serviceCategory) {
+        DialogPane.Dialog<ButtonType> dialog = dialogPane.showConfirmation("XÁC NHẬN", "Bạn có chắc chắn muốn xóa loại dịch vụ này?");
+        dialog.onClose(buttonType -> {
+            if (buttonType == ButtonType.YES) {
+                Task<Void> deleteTask = new Task<>() {
+                    @Override
+                    protected Void call() {
+                        ServiceCategoryDAO.deleteData(serviceCategory.getServiceCategoryID());
+                        return null;
+                    }
+                };
+
+                deleteTask.setOnRunning(e -> {
+                    addBtn.setDisable(true);
+                    updateBtn.setDisable(true);
+                });
+
+                deleteTask.setOnSucceeded(e -> {
+                    addBtn.setDisable(false);
+                    updateBtn.setDisable(false);
+                    loadData();
+                    dialogPane.showInformation("Xóa thành công", "Loại dịch vụ đã được xóa");
+                });
+
+                Thread deleteThread = new Thread(deleteTask);
+                deleteThread.setDaemon(true);
+                deleteThread.start();
+            }
+        });
+    }
+
+    private void handleUpdateBtn(ServiceCategory serviceCategory) {
+        serviceCategoryNameTextField.setText(serviceCategory.getServiceCategoryName());
+        serviceCategoryIDTextField.setText(serviceCategory.getServiceCategoryID());
+
+        String iconName = serviceCategory.getIcon();
+        iconSelector.getSelectionModel().select(
+                iconServiceMap.entrySet().stream()
+                        .filter(entry -> entry.getValue().equals(iconName))
+                        .map(Map.Entry::getKey)
+                        .findFirst().orElse(null)
+        );
+
+        addBtn.setManaged(false);
+        addBtn.setVisible(false);
+        updateBtn.setManaged(true);
+        updateBtn.setVisible(true);
+    }
+
+    private void handleUpdateAction() {
+        try {
+            String serviceCategoryID = serviceCategoryIDTextField.getText();
+            String serviceCategoryName = serviceCategoryNameTextField.getText();
+            Image selectedIcon = iconSelector.getSelectionModel().getSelectedItem();
+            String iconName = iconServiceMap.get(selectedIcon);
+
+            ServiceCategory serviceCategory = new ServiceCategory(serviceCategoryID, serviceCategoryName, ObjectStatus.ACTIVE, iconName);
+
+            DialogPane.Dialog<ButtonType> dialog = dialogPane.showConfirmation("XÁC NHẬN", "Bạn có chắc chắn muốn cập nhật loại dịch vụ này?");
+            dialog.onClose(buttonType -> {
+                if (buttonType == ButtonType.YES) {
+                    Task<Void> updateTask = new Task<>() {
+                        @Override
+                        protected Void call() {
+                            ServiceCategoryDAO.updateData(serviceCategory);
+                            return null;
+                        }
+                    };
+
+                    updateTask.setOnRunning(e -> {
+                        addBtn.setDisable(true);
+                        updateBtn.setDisable(true);
+                    });
+
+                    updateTask.setOnSucceeded(e -> {
+                        addBtn.setDisable(false);
+                        updateBtn.setDisable(false);
+                        handleResetAction();
+                        loadData();
+                        dialogPane.showInformation("Cập nhật thành công", "Loại dịch vụ đã được cập nhật");
+                    });
+
+                    Thread updateThread = new Thread(updateTask);
+                    updateThread.setDaemon(true);
+                    updateThread.start();
+                }
+            });
+
+        } catch (IllegalArgumentException e) {
+            dialogPane.showWarning("LỖI", e.getMessage());
+        }
+    }
+
+    private void handleSearchAction() {
+        String searchText = serviceCategoryIDSearchField.getValue();
+
+        Task<ObservableList<ServiceCategory>> searchTask = new Task<>() {
+            @Override
+            protected ObservableList<ServiceCategory> call() {
+                List<ServiceCategory> serviceCategories = searchText == null || searchText.isEmpty()
+                        ? ServiceCategoryDAO.findAll()
+                        : ServiceCategoryDAO.findDataByContainsId(searchText);
+                return FXCollections.observableArrayList(serviceCategories);
+            }
+        };
+
+        searchTask.setOnSucceeded(e -> {
+            items = searchTask.getValue();
+            serviceCategoryTableView.setItems(items);
+            serviceCategoryTableView.refresh();
+
+            if (items.size() == 1) {
+                ServiceCategory serviceCategory = items.getFirst();
+                serviceCategoryNameSearchField.setText(serviceCategory.getServiceCategoryName());
+            } else {
+                serviceCategoryNameSearchField.setText(null);
+            }
+        });
+
+        Thread searchThread = new Thread(searchTask);
+        searchThread.setDaemon(true);
+        searchThread.start();
+    }
+
+    private void setupIconSelector() {
+        List<String> iconPaths = List.of(
+                "/iuh/fit/icons/service_icons/ic_car.png",
+                "/iuh/fit/icons/service_icons/ic_cleaning.png",
+                "/iuh/fit/icons/service_icons/ic_food.png",
+                "/iuh/fit/icons/service_icons/ic_karaoke.png",
+                "/iuh/fit/icons/service_icons/ic_massage.png",
+                "/iuh/fit/icons/service_icons/ic_meeting.png",
+                "/iuh/fit/icons/service_icons/ic_washing.png"
+        );
+
+        List<String> services = List.of("car", "cleaning", "food", "karaoke", "massage", "meeting", "washing");
+
+        ObservableList<Image> icons = FXCollections.observableArrayList();
+        for (int i = 0; i < iconPaths.size(); i++) {
+            Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream(iconPaths.get(i))));
+            icons.add(icon);
+            iconServiceMap.put(icon, services.get(i));
+        }
+
+        iconSelector.setItems(icons);
+        iconSelector.setCellFactory(comboBox -> new ListCell<>() {
+            @Override
+            protected void updateItem(Image item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setGraphic(null);
+                } else {
+                    ImageView imageView = new ImageView(item);
+                    imageView.setFitWidth(24);
+                    imageView.setFitHeight(24);
+                    setGraphic(imageView);
+                }
+            }
+        });
+
+        iconSelector.setButtonCell(new ListCell<>() {
+            @Override
+            protected void updateItem(Image item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setGraphic(null);
+                } else {
+                    ImageView imageView = new ImageView(item);
+                    imageView.setFitWidth(24);
+                    imageView.setFitHeight(24);
+                    setGraphic(imageView);
+                }
+            }
+        });
+
+    }
+}
--- src/main/java/iuh/fit/dao/ServiceCategoryDAO.java
+++ src/main/java/iuh/fit/dao/ServiceCategoryDAO.java
@@ -1,75 +1,141 @@
 package iuh.fit.dao;
 
 import iuh.fit.models.ServiceCategory;
+import iuh.fit.models.enums.ObjectStatus;
 import iuh.fit.utils.EntityManagerUtil;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 
-import java.security.spec.ECField;
 import java.util.List;
 
 public class ServiceCategoryDAO {
-    public static boolean create(ServiceCategory sc){
-        try(EntityManager em = EntityManagerUtil.getEntityManager()){
-            em.getTransaction().begin();
-            em.persist(sc);
-            em.getTransaction().commit();
-            return true;
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }
-    }
 
-    public static boolean delete(String id){
-        try(EntityManager em = EntityManagerUtil.getEntityManager()){
-            em.getTransaction().begin();
-            ServiceCategory sc = em.find(
-                    ServiceCategory.class,
-                    id
-            );
-            if(sc == null)
-                return false;
-            em.remove(sc);
-            em.getTransaction().commit();
-            return true;
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static boolean update(ServiceCategory newInfor){
-        try(EntityManager em = EntityManagerUtil.getEntityManager()){
-            em.getTransaction().begin();
-            em.merge(newInfor);
-            em.getTransaction().commit();
-            return true;
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static List<ServiceCategory> findAll(){
-        try(EntityManager em = EntityManagerUtil.getEntityManager()){
-            TypedQuery<ServiceCategory> query = em.createQuery(
-                    "select sc from ServiceCategory sc",
-                        ServiceCategory.class
-            );
-
-            return query.getResultList();
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static ServiceCategory findById(String id){
+    public static boolean createData(ServiceCategory serviceCategory) {
         try (EntityManager em = EntityManagerUtil.getEntityManager()) {
-            return em.find(
-                    ServiceCategory.class,
-                    id
-            );
-        }catch (Exception e){
-            throw new RuntimeException(e);
+            em.getTransaction().begin();
+            em.persist(serviceCategory);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean deleteData(String serviceCategoryID) {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            em.getTransaction().begin();
+            ServiceCategory sc = em.find(ServiceCategory.class, serviceCategoryID);
+            if (sc == null) return false;
+            sc.setIsActivate(ObjectStatus.INACTIVE);
+            em.merge(sc);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean updateData(ServiceCategory serviceCategory) {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            em.getTransaction().begin();
+            em.merge(serviceCategory);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static List<ServiceCategory> findAll() {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            TypedQuery<ServiceCategory> query = em.createQuery(
+                    "SELECT sc FROM ServiceCategory sc WHERE sc.isActivate = 'ACTIVE'", ServiceCategory.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static ServiceCategory findById(String serviceCategoryID) {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            return em.find(ServiceCategory.class, serviceCategoryID);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static boolean isServiceCategoryInUse(String serviceCategoryID) {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            TypedQuery<Long> query = em.createQuery(
+                    """
+                    SELECT COUNT(r.id) FROM Room r 
+                    JOIN r.roomCategory rc 
+                    WHERE rc.roomCategoryID = :serviceCategoryID 
+                    AND r.roomStatus IN ('ON_USE', 'OVERDUE')
+                    """, Long.class);
+            query.setParameter("serviceCategoryID", serviceCategoryID);
+            return query.getSingleResult() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Find ServiceCategories by partial ID
+    public static List<ServiceCategory> findDataByContainsId(String input) {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            TypedQuery<ServiceCategory> query = em.createQuery(
+                    "SELECT sc FROM ServiceCategory sc WHERE LOWER(sc.serviceCategoryID) LIKE :input AND sc.isActivate = 'ACTIVATE'",
+                    ServiceCategory.class);
+            query.setParameter("input", "%" + input.toLowerCase() + "%");
+            return query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Get the top 3 ServiceCategory IDs
+    public static List<String> getTopThreeID() {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            TypedQuery<String> query = em.createQuery(
+                    "SELECT sc.serviceCategoryID FROM ServiceCategory sc WHERE sc.isActivate = 'ACTIVATE' ORDER BY sc.serviceCategoryID DESC",
+                    String.class);
+            query.setMaxResults(3);
+            return query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Get the next ServiceCategory ID from GlobalSequence
+    public static String getNextServiceCategoryID() {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            String queryStr = "SELECT gs.nextID FROM GlobalSequence gs WHERE gs.tableName = 'ServiceCategory'";
+            TypedQuery<String> query = em.createQuery(queryStr, String.class);
+            return query.getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "SC-000001";
+        }
+    }
+
+    // Get list of ServiceCategory names
+    public static List<String> getServiceCategoryNames() {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            TypedQuery<String> query = em.createQuery(
+                    "SELECT sc.serviceCategoryName FROM ServiceCategory sc WHERE sc.isActivate = 'ACTIVATE'",
+                    String.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
