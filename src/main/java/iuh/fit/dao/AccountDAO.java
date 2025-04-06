2bf45b3e0ba7ac5951037091154490706409c4f2
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-04-06 17:22:17 +0700

    cai thien code

--- src/main/java/iuh/fit/dao/AccountDAO.java
+++ src/main/java/iuh/fit/dao/AccountDAO.java
@@ -1,123 +1,182 @@
 package iuh.fit.dao;
 
 import iuh.fit.models.Account;
+import iuh.fit.models.Employee;
+import iuh.fit.models.enums.AccountStatus;
 import iuh.fit.utils.EntityManagerUtil;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.TypedQuery;
 
-import java.util.Collections;
 import java.util.List;
 
-/**
- * Admin 1/16/2025
- **/
 public class AccountDAO {
-    public static void create(Account account) {
+
+    // Lấy danh sách tất cả tài khoản
+    public static List<Account> getAccount() {
         try (EntityManager em = EntityManagerUtil.getEntityManager()) {
-            try{
-
-                em.getTransaction().begin();
-                em.persist(account);
-                em.getTransaction().commit();
-
-            }catch(Exception transactionException) {
-
-                transactionException.printStackTrace();
-                em.getTransaction().rollback();
-
-            }
-        }  catch (Exception resourceException) {
-
-            resourceException.printStackTrace();
-
-        }
-    }
-
-    public static List<Account> findAll() {
-
-        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
-
-            TypedQuery<Account> query = em.createQuery("select a from Account a", Account.class);
+            String jpql = """
+                SELECT a FROM Account a
+                JOIN FETCH a.employee e
+                """;
+            TypedQuery<Account> query = em.createQuery(jpql, Account.class);
             return query.getResultList();
-
-        }  catch (Exception resourceException) {
-
-            resourceException.printStackTrace();
-            return Collections.emptyList();
-
-        }
-    }
-
-    public static Account findById(String id) {
-        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
-
-            return em.find(Account.class, id);
-
-        }  catch (Exception resourceException) {
-
-            resourceException.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
             return null;
-
         }
     }
 
-    public static void update(Account account) {
+    // Lấy tài khoản theo ID
+    public static Account getDataByID(String accountID) {
         try (EntityManager em = EntityManagerUtil.getEntityManager()) {
-            try{
+            return em.find(Account.class, accountID);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-                em.getTransaction().begin();
-                em.merge(account);
-                em.getTransaction().commit();
+    // Lấy tài khoản khi đăng nhập
+    public static Account getLogin(String username, String password) {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            String jpql = """
+                SELECT a FROM Account a
+                JOIN FETCH a.employee e
+                WHERE a.userName = :username
+                """;
+            TypedQuery<Account> query = em.createQuery(jpql, Account.class);
+            query.setParameter("username", username);
+            Account account = query.getSingleResult();
 
-            }catch(Exception transactionException) {
-
-                transactionException.printStackTrace();
-                em.getTransaction().rollback();
-
+            if (account != null && account.getPassword().equals(password)) {
+                return account;
             }
-        }  catch (Exception resourceException) {
-
-            resourceException.printStackTrace();
-
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
-    public static void delete(String id) {
+    // Tạo mới tài khoản
+    public static void createData(Account account) {
         try (EntityManager em = EntityManagerUtil.getEntityManager()) {
-            try{
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
 
-                em.getTransaction().begin();
-
-                Account account = em.find(Account.class, id);
-                if (account != null) em.remove(account);
-
-                em.getTransaction().commit();
-
-            }catch(Exception transactionException) {
-
-                transactionException.printStackTrace();
-                em.getTransaction().rollback();
-
+            // Đảm bảo đối tượng Employee được quản lý (managed)
+            if (!em.contains(account.getEmployee())) {
+                account.setEmployee(em.merge(account.getEmployee()));
             }
-        }  catch (Exception resourceException) {
 
-            resourceException.printStackTrace();
+            // Lấy ID tiếp theo
+            String nextID = getNextAccountID();
+            account.setAccountID(nextID);
 
+            // Cập nhật ID tiếp theo
+            updateNextAccountID(nextID);
+
+            // Lưu đối tượng Account
+            em.persist(account);
+            tx.commit();
+            System.out.println("Tạo tài khoản thành công với mã: " + nextID);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public static Account getLogin(String userName, String hashedPassword) {
+
+    // Xóa tài khoản
+    public static void deleteData(String accountID) {
         try (EntityManager em = EntityManagerUtil.getEntityManager()) {
-            TypedQuery<Account> query = em.createQuery(
-                    "SELECT a FROM Account a WHERE a.userName = :userName AND a.password = :password",
-                    Account.class
-            );
-            query.setParameter("userName", userName);
-            query.setParameter("password", hashedPassword);
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
+            Account account = em.find(Account.class, accountID);
+            if (account != null) {
+                em.remove(account);
+                tx.commit();
+                System.out.println("Xóa tài khoản thành công: " + accountID);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-            List<Account> result = query.getResultList();
-            return result.isEmpty() ? null : result.get(0);
+    // Cập nhật tài khoản
+    public static void updateData(Account account) {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            EntityTransaction tx = em.getTransaction();
+            tx.begin();
+            em.merge(account);
+            tx.commit();
+            System.out.println("Cập nhật tài khoản thành công: " + account.getAccountID());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    // Lấy ID tiếp theo từ GlobalSequence
+    public static String getNextAccountID() {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            String jpql = "SELECT gs.nextID FROM GlobalSequence gs WHERE gs.tableName = 'Account'";
+            TypedQuery<String> query = em.createQuery(jpql, String.class);
+            return query.getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "ACC-000001";
+        }
+    }
+
+    // Cập nhật ID tiếp theo
+    private static void updateNextAccountID(String currentNextID) {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            String prefix = "ACC-";
+            int nextIDNum = Integer.parseInt(currentNextID.substring(prefix.length())) + 1;
+            String newNextID = prefix + String.format("%06d", nextIDNum);
+
+            String jpql = """
+                UPDATE GlobalSequence gs
+                SET gs.nextID = :newNextID
+                WHERE gs.tableName = 'Account'
+                """;
+            em.getTransaction().begin();
+            em.createQuery(jpql)
+                    .setParameter("newNextID", newNextID)
+                    .executeUpdate();
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Lấy tài khoản theo mã nhân viên
+    public static Account getAccountByEmployeeID(String employeeID) {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            String jpql = """
+                SELECT a FROM Account a
+                WHERE a.employee.employeeCode = :employeeID
+                """;
+            TypedQuery<Account> query = em.createQuery(jpql, Account.class);
+            query.setParameter("employeeID", employeeID);
+            return query.getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Tìm tài khoản theo ID chứa từ khóa
+    public static List<Account> findDataByContainsId(String input) {
+        try (EntityManager em = EntityManagerUtil.getEntityManager()) {
+            String jpql = """
+                SELECT a FROM Account a
+                JOIN FETCH a.employee e
+                WHERE LOWER(a.accountID) LIKE :input
+                """;
+            TypedQuery<Account> query = em.createQuery(jpql, Account.class);
+            query.setParameter("input", "%" + input.toLowerCase() + "%");
+            return query.getResultList();
         } catch (Exception e) {
             e.printStackTrace();
             return null;
