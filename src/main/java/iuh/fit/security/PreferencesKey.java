517ccf7770df763dc9c582e5868d6f118e036588
phatdang <phatdang19052004@gmail.com>
2025-04-13 16:49:58 +0700

    them PreferencesKey

---
+++ src/main/java/iuh/fit/security/PreferencesKey.java
@@ -0,0 +1,29 @@
+package iuh.fit.security;
+
+public class PreferencesKey {
+    // default value when user have not ever choosed file path
+    public static final String  DEFAULT_VALUE = "None";
+    // file path default when user have not ever choosed file path
+    public static final String  DEFAULT_FILE_PATH = "Chưa chọn thư mục lưu trữ";
+    // key for backup data address
+    public static final String BACK_UP_DATA_FILE_ADDRESS_KEY = "BACK_UP_DATA";
+    public static final String BACK_UP_FULL_HAND_ADDRESS_KEY = "BACK_UP_FULL_HAND_DATA";
+
+    public static final String BACK_UP_OPTION_KEY = "BACK_UP_OPTION";
+    public static final String BACK_UP_FORM_AUTO_VALUE = "AUTO";
+    public static final String BACK_UP_FORM_WARNING_VALUE = "WARNING";
+    public static final String BACK_UP_FORM_NO_VALUE = "NO";
+
+    public static final String BACKUP_COMPRESS_FILE = "COMPRESS_FILE";
+
+    public static final String EXPORT_INVOICE_STATISTIC = "INVOICE";
+    public static final String EXPORT_SERVICE_STATISTIC = "SERVICE";
+    public static final String EXPORT_ROOM_STATISTIC = "ROOM";
+
+    public static final String RESTORE_FILE = "RESTORE_FILE";
+
+    public static final String CURRENT_USING_DATA = "CURRENT_USING_DATA";
+
+    public static final String USING_BACKUP_FUCTION = "USING_BACKUP_FUCTION";
+
+}
